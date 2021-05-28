40818e6c70394706bf9d0882871d193266ede756
Petro Davybida <petdavy1991@gmail.com>
2021-05-28 22:20:41 +0300

    add bulma

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -3,12 +3,35 @@
 </script>
 
 <nav>
-	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
+	<ul class="navbar-menu">
+		<div class="navbar-start">
+			<li>
+				<a
+					class="navbar-item"
+					aria-current="{segment === undefined ? 'page' : undefined}"
+					href=".">
+						home
+					</a>
+			</li>
+			<li>
+				<a
+					class="navbar-item"
+					aria-current="{segment === 'about' ? 'page' : undefined}"
+					href="about"
+				>
+					about
+				</a>
+			</li>
+			<li>
+				<a
+					class="navbar-item"
+					rel=prefetch
+					aria-current="{segment === 'blog' ? 'page' : undefined}" 
+					href="blog"
+				>
+					blog
+				</a>
+			</li>
+		</div>
 	</ul>
 </nav>
